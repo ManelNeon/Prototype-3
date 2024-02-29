ea5bfde78a2cf49953adf5c0a8c1665f5c63ee59
ManelNeon <neonextra0@gmail.com>
2024-02-29 18:01:38 +0000

    Lesson 3.4
    Helpfull, não fazia o áudio assim, mas a partir de agora farei, muito helpful

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,9 +18,24 @@ public class PlayerController : MonoBehaviour
 
     Animator playerAnim;
 
+    [Header("Particle")]
+
+    [SerializeField] ParticleSystem explosionParticle;
+
+    [SerializeField] ParticleSystem runningParticle;
+
+    [Header("Audio Clips")]
+
+    [SerializeField] AudioClip jumpSound;
+
+    [SerializeField] AudioClip crashSound;
+
+    AudioSource playerAudio;
+
     void Start()
     {
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         gameOver = false;
@@ -30,6 +45,8 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && grounded && !gameOver)
         {
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            runningParticle.Stop();
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             grounded = false;
             playerAnim.SetTrigger("Jump_trig");
@@ -40,6 +57,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            runningParticle.Play();
             grounded = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
@@ -48,6 +66,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            runningParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
